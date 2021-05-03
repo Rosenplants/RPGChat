06983382a938323c08a6a1a33dc6ab3709c1cba4
Sarah Zerod <szerod@gmail.com>
2021-05-03 11:36:21 -0400

    bugfix: database connection

--- server/db/db.js
+++ server/db/db.js
@@ -8,15 +8,17 @@ const config = {
   logging: false,
 };
 
-// if (process.env.DATABASE_URL) {
-//   config.dialectOptions = {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// }
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
-// process.env.DATABASE_URL ||
-const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, config);
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+);
 
 module.exports = db;
